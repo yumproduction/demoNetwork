5b1bac61a62291a38b837906290d68af762a9fa6
max <salimova.mekhri@gmail.com>
2020-07-06 16:13:39 +0500

    Updates

---
+++ precache-manifest.50d4a30602a7612948d0542bbba98552.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89fead0c9c70c415d968666385a1692",
+    "url": "/demoNetwork/index.html"
+  },
+  {
+    "revision": "df9ce64371553571d2bf",
+    "url": "/demoNetwork/static/css/3.b11f7595.chunk.css"
+  },
+  {
+    "revision": "a4929c31189f15821bf2",
+    "url": "/demoNetwork/static/css/main.7cdaed6b.chunk.css"
+  },
+  {
+    "revision": "d0ffc2f25cd8c89ff9be",
+    "url": "/demoNetwork/static/js/2.32a36136.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demoNetwork/static/js/2.32a36136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9ce64371553571d2bf",
+    "url": "/demoNetwork/static/js/3.f320fd91.chunk.js"
+  },
+  {
+    "revision": "a4929c31189f15821bf2",
+    "url": "/demoNetwork/static/js/main.da786f0c.chunk.js"
+  },
+  {
+    "revision": "7c699bce2e328ca77ee8",
+    "url": "/demoNetwork/static/js/runtime-main.10395c28.js"
+  },
+  {
+    "revision": "1f0524e0b44ecb94c22fff9e33991186",
+    "url": "/demoNetwork/static/media/logo.1f0524e0.png"
+  },
+  {
+    "revision": "fc29bc18868c7a13375b98579bd8e873",
+    "url": "/demoNetwork/static/media/moon.fc29bc18.png"
+  },
+  {
+    "revision": "d932485dacc691aa2eea99c87cc1b74c",
+    "url": "/demoNetwork/static/media/preload.d932485d.svg"
+  },
+  {
+    "revision": "5b98826e80c07c1edfea91686613eaca",
+    "url": "/demoNetwork/static/media/sun.5b98826e.png"
+  },
+  {
+    "revision": "220a706279be0038377319129fe76716",
+    "url": "/demoNetwork/static/media/user.220a7062.png"
+  }
+]);
